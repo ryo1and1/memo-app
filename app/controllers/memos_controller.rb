6f7bd31836018b7e75e10abaff6db559e1e6f9fa
EC2 Default User <ec2-user@ip-172-31-38-179.us-east-2.compute.internal>
2018-10-15 12:29:14 +0000

    initial commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,16 @@
+class MemosController < ApplicationController
+    def index
+        #トップページが呼ばれたときに動作するアクション
+        @memos=Memo.all
+    end
+    
+    def new
+        #新規作成ページが呼ばれたときに動作するアクション
+    end
+    
+    def create
+        #新しいメモがフォームからPostされた時に動作するアクション
+        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+        redirect_to "/"
+    end
+end
